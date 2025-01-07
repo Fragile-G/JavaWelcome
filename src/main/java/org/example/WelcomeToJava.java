f8a5daf0566d0e71e83de542fb374a698e98f6be
Fragile-G <olasunkayode@gmail.com>
2025-01-07 15:15:56 +0200

    commit

---
+++ src/main/java/org/example/WelcomeToJava.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class WelcomeToJava {
+    public static void main(String[] args) {
+        // I am printing welcome messages
+        System.out.println("Welcome To Java Programming 1");
+        System.out.println("Welcome To Java Programming 2");
+        System.out.println("Welcome To Java Programming 3");
+        System.out.println("Welcome To Java Programming 4");
+        System.out.println("Welcome To Java Programming 5");
+
+        /*
+        jhgdhdkjfd
+        dkfjdljdklfd
+        fsdkfjsdlflsdf
+        sdkfsdlf
+        sdfsdfsd;fd
+         */
+
+        System.out.println("Welcome To Java Programming 6");
+
+    }
+}
